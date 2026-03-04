2233aab64fedc063c59b354a3101b841e3cca6f4
Tahir <tahir.tamam.005@gmail.com>
2026-03-04 20:12:26 +0530

    backend updates

--- BLUECARBONPROJECT-main/server/index.ts
+++ BLUECARBONPROJECT-main/server/index.ts
@@ -1,6 +1,12 @@
 import dotenv from "dotenv";
 dotenv.config();
 
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 import express, { type Request, Response, NextFunction } from "express";
 import helmet from "helmet";
 import rateLimit from "express-rate-limit";
